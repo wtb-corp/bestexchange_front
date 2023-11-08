dbddd4e4bab474f8e3a23bf322ce336a6d1a17a9
Alexeev Vadim <alexeev.vadim@challenge.day>
2023-11-08 14:37:25 +0700

    first update
    fast

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Container, Typography } from '@mui/material'
 import { useTelegram } from './hooks/useTelegram'
 import SuperSelect from './components/SuperSelect'
+import GetLisr from './components/GetLisr'
+import ResultList from './components/ResultList'
+
+const {tg, onToggleButton} = useTelegram();
 
 function App() {
-  
-  const {tg} = useTelegram()
+  const [isShown, setIsShown] = useState(false);
 
-  useEffect( () => {
-    tg.ready()
-  }, [])
+  const handleClick = event => {
+    setIsShown(current => !current);
+  };
+
+  useEffect( () => { 
+    tg.ready();
+    tg.MainButton.onClick(() => {
+      tg.sendData("Привет бэкенд!"); 
+      //при клике на основную кнопку отправляем данные в строковом виде
+      tg.MainButton.setText("Привет бэкенд!");
+      tg.MainButton.color = "#FDFDFD";
+      tg.MainButton.textColor = "#CCCCCC";
+      tg.MainButton.disable();
+      handleClick();
+    });
+  }, []);
 
   const boxStyles = {
     background: "#fdfdfd",
@@ -22,10 +38,13 @@ function App() {
     position: "relative"
   }
 
+  onToggleButton();
+
   return (
     <Container maxWidth="xs" sx={boxStyles}>
-      <Typography variant="h6" component="h2" gutterBottom>EX beta v.0.1</Typography>
-      <SuperSelect />
+      <Typography variant="h6" component="h2" gutterBottom>BestExChanger beta v.0.3</Typography>
+      <GetLisr />
+      {isShown && <ResultList />}
     </Container>
   )
 }
